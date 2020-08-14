698339caf75361b278b7dd4e5533bd78c0ed92c1
joe williamson <joey8595@hotmail.co.uk>
2020-08-14 16:17:42 +0100

    l

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+dose.rb
+
+
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :cocktail, uniqueness: { scope: :ingredient }
+end
---
+++ db/migrate/20200814151527_doses.rb
@@ -0,0 +1,4 @@
+class Doses < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
